e0c4924c1595465ae8bace11fa0392820a7c969d
j9972 <j9972@naver.com>
2020-10-25 22:15:53 +0900

    지뢰를 심는 부분의 코드에서 textContent를 심는 부분의 children이 계속 오류뜬다....

--- 7.2마인/마인.js
+++ 7.2마인/마인.js
@@ -12,16 +12,18 @@ var codeGraph = {
     mineScore: 1,
     normal: 0,
 }
+var res = document.querySelector('#result');
 
 document.querySelector('#exec').addEventListener('click', (e) => {
     tbody.innerHTML = '';
+    dataSet = [];
+    op = 0;
     stopingFlag = false;
-    var res = document.querySelector('#result').textContent = '';
+    res.textContent = '';
     
-
-    var hor = parent(document.querySelector('#hor').value);
-    var ver = parent(document.querySelector('#ver').value);
-    var mine = parent(document.querySelector('#mine').value);
+    var hor = parseInt(document.querySelector('#hor').value);
+    var ver = parseInt(document.querySelector('#ver').value);
+    var mine = parseInt(document.querySelector('#mine').value);
     console.log(hor,ver,mine);
 
     // 위치
@@ -30,7 +32,7 @@ document.querySelector('#exec').addEventListener('click', (e) => {
     });
 
     var shuffle = [];
-    for(var i = 0; i < sub.length; i++) {
+    while(sub.length > hor * ver - mine) {
         var moved = sub.splice(Math.floor(Math.random() * sub.length), 1)[0];
         shuffle.push(moved);
     }
@@ -40,76 +42,62 @@ document.querySelector('#exec').addEventListener('click', (e) => {
     for(var i = 0; i < ver; i++){
         var arr = [];
         var tr = document.createElement('tr');
-        dataSet.push(tr);
+        dataSet.push(arr);
         for(var i = 0; i < hor; i++) {
-            arr.push(codeGraph.normal);
             var td = document.createElement('td');
+            arr.push(codeGraph.normal);
 
             // 우클릭
             td.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
                 if(stopingFlag) {
                     return;
                 }
-                e.preventDefault();
 
                 var parentTr = e.currentTarget.parentNode;
                 var parentTbody = e.currentTarget.parentNode.parentNode;
                 var box = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
                 var line = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+
+                if(dataSet[line][box] === codeGraph.opened) {
+                    return;
+                } 
 
                 // 점수도 바꿔야 하고 클래스도 바꾸고 이미지도 바꿔야 한다.
                 if(e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X') {
-                    currentTarget.add('flag');
-                    currentTarget.textContent = '!';
-                    if(e.currentTarget === codeGraph.mineScore) {
-                        e.currentTarget = codeGraph.flagMine;
+                    e.currentTarget.classList.add('flag');
+                    e.currentTarget.textContent = '!';
+                    if(dataSet[line][box] === codeGraph.mineScore) {
+                        dataSet[line][box] = codeGraph.flagMine;
                     }
                     else {
-                        e.currentTarget = codeGraph.flag;
+                        dataSet[line][box] = codeGraph.flag;
                     }
                 }
                 else if(e.currentTarget.textContent === '!') {
-                    currentTarget.remove('flag');
-                    currentTarget.add('question');
-                    currentTarget.textContent = '?';
-                    if() {
-
+                    e.currentTarget.classList.remove('flag');
+                    e.currentTarget.classList.add('question');
+                    e.currentTarget.textContent = '?';
+                    if(dataSet[line][box] === codeGraph.flagMine) {
+                        dataSet[line][box] = codeGraph.questMine;
                     } else {
-
+                        dataSet[line][box] = codeGraph.questMark;
                     }
                 }
                 else if(e.currentTarget.textContent === '?') {
-                    currentTarget.remove('question');
-                    currentTarget.textContent = '' || currentTarget.textContent = 'X';
-                }
-
-                if(dataSet[line][box].textContent === '' || dataSet[line][box].textContent === 'X') {
-
-                    dataSet[line][box] = flagMine;
-                }
-                else {
-                    dataSet[line][box].textContent === '!'
-                    dataSet[line][box] = flag;
-                }
-
-                if(dataSet[line][box].textContent === '!') {
-                    dataSet[line][box].remove('flag');
-                    dataSet[line][box].add('question');
-                    dataSet[line][box].textContent === '?'
-                }
-                else {
-
-                }
-                if (dataSet[line][box].textContent === '?') {
-                    dataSet[line][box].remove('question');
-                    dataSet[line][box].textContent === '' || dataSet[line][box].textContent === 'X';
-                }
-                else {
-                }
+                    e.currentTarget.classList.remove('question');
+                    if(dataSet[line][box] === codeGraph.questMine) {
+                        e.currentTarget.textContent = 'X';
+                        dataSet[line][box] = codeGraph.mineScore;
+                    } else {
+                        e.currentTarget.textContent = '';
+                        dataSet[line][box] = codeGraph.normal;
+                    }
+                }s
             });
 
             // 좌클릭
-            document.addEventListener('click', (e) => {
+            td.addEventListener('click', (e) => {
                 if(stopingFlag) {
                     return;
                 }
@@ -119,19 +107,81 @@ document.querySelector('#exec').addEventListener('click', (e) => {
                 var box = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
                 var line = Array.prototype.indexOf.call(parentTbody.children, parentTr);
 
-            });
+                if([codeGraph.opened, codeGraph.flagMine, codeGraph.questMine, codeGraph.flag, codeGraph.questMark].includes(dataSet[line][box])) {
+                    return;
+                }
 
+                e.currentTarget.classList.add('opened');
+                op++;
+
+                if(dataSet[line][box] === codeGraph.mineScore) {
+                    e.currentTarget.textContent = '펑';
+                    res.textContent = '실패';
+                    stopingFlag = true;
+                }
+                else {
+                    var neighbor = [ dataSet[line][box - 1], dataSet[line][box + 1] ];
+                    if(dataSet[line - 1]) {
+                        neighbor = neighbor.concat(dataSet[line - 1][box - 1], dataSet[line - 1][box], dataSet[line - 1][box + 1]);
+                    }
+                    if(dataSet[line + 1]) {
+                        neighbor = neighbor.concat(dataSet[line + 1][box - 1], dataSet[line + 1][box], dataSet[line + 1][box + 1]);
+                    }
+                    var neighborMineNum = neighbor.filter((v) => {
+                        return  [codeGraph.flagMine, codeGraph.questMine, codeGraph.mineScore].includes(v);
+                    }).length;
+                    e.currentTarget.textContent = '' || neighborMineNum;
+                    dataSet[line][box] = codeGraph.opened;
+
+                    if(neighborMineNum === 0) {
+                        var neighborBox =[];
+                        if(tbody.children[line - 1]) {
+                            neighborBox = neighborBox.concat([
+                                tbody.children[line - 1][box - 1], 
+                                tbody.children[line - 1][box], 
+                                tbody.children[line - 1][box + 1]
+                            ]);
+                        }
+                        neighborBox = neighborBox.concat([
+                            tbody.children[line][box - 1], 
+                            tbody.children[line][box + 1]
+                        ]);
+                        if(tbody.children[line + 1]) {
+                            neighborBox = neighborBox.concat([
+                                tbody.children[line + 1][box - 1], 
+                                tbody.children[line + 1][box], 
+                                tbody.children[line + 1][box + 1]
+                            ]);
+                        }
+                        neighborBox.filter((v) => !!v).forEach((sideBox) => {
+                            var parentTr = e.currentTarget.parentNode;
+                            var parentTbody = e.currentTarget.parentNode.parentNode;
+                            var ssidebox = Array.prototype.indexOf.call(parentTr.children, sideBox);
+                            var ssideline = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+                            if(dataSet[ssideline][ssidebox] !== codeGraph.mineScore) {
+                                sideBox.click();
+                            }
+                        });
+                    }
+                }
+                if(op === ver * hor - mine) {
+                    stopingFlag = true;
+                    res.textContent ='승리';
+                }
+            });
             tr.appendChild(td);
-        }
+        };
         tbody.appendChild(tr);
-    }
+    };
 
     // 심기
-    for(var k = 0; k < ver * hor - mine; k++) {
-        var h = Math.floor(shuffle[k] / ver);
-        var w = shuffle[k] % 10;
-        sub.children[h].children[w] = 'X';
-        dataSet[line][box].textContent = codeGraph.mineScore;
+    for(var k = 0; k < shuffle.length; k++) { // ex shuffle[k] === 59
+        // 00 01 02 03 04
+        // 10 11 12 13 14
+        // 20 21 22 23 24
+        var h = Math.floor(shuffle[k] / hor); // floor 반내림 
+        var w = shuffle[k] % ver;// 0 ~ 9 ( % === 나머지 )
+        tbody.children[h].children[w].textContent = 'X';
+        dataSet[h][w] = codeGraph.mineScore;
     }
-
 })
