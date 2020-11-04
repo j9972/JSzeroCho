7303171c5f8891c0c98afe86d98b3968d784728c
j9972 <j9972@naver.com>
2020-11-04 13:13:54 +0900

    변수설정

---
+++ 10.1jasstone/1-jasstone.js
@@ -0,0 +1,24 @@
+var partner = {
+    hero: document.querySelector('#rival-hero'),
+    deck: document.querySelector('#rival-deck'),
+    field: document.querySelector('#rival-cards'),
+    cost: document.querySelector('#rival-cost'),
+    deckData: [],
+    heroData: [],
+    fieldData: [],
+    clicked: null,
+    clickedData: null,
+  };
+  
+  var me = {
+    hero: document.querySelector('#my-hero'),
+    deck: document.querySelector('#my-deck'),
+    field: document.querySelector('#my-cards'),
+    cost: document.querySelector('#my-cost'),
+    deckData: [],
+    heroData: [],
+    fieldData: [],
+    clicked: null,
+    clickedData: null,
+  };
+
