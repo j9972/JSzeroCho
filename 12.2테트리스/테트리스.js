f4de71065e42bcfe6837dae557580e492a95a6c6
j9972 <j9972@naver.com>
2020-11-08 15:50:07 +0900

    현재상황

--- 12.2테트리스/테트리스.js
+++ 12.2테트리스/테트리스.js
@@ -200,3 +200,91 @@ var blocks = [
 ];
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'violet'];
+
+const activeBlock = value => (value > 0 && value < 10);
+const InvalidBlock = value => (value === undefined && value > 10);
+
+function init() {
+    const fragment = document.createDocumentFragment();
+    [...Array(20).fill()].forEach((col,i) => {
+        const tr = document.createElement('tr');
+        fragment.appendChild(tr);
+        [...Array(10).fill()].forEach((row,j) => {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+        });
+        // 실수 1. fill() 인자에 0을 넣지 않았고 테트리스 데이터에 column을 push를 해야하는데 appendChild를 함
+        const column = Array(10).fill(0);
+        tetrisData.push(column);
+    })
+    tetris.appendChild(fragment);
+}
+
+// 문제 1
+function draw() {
+    tetris.forEach((col,i) => {
+        col.forEach((row,j) => {
+            if(row > 0) { // 문제 1. 뒷 부분 어떻게 색을 넣나?
+                tetris.children[i].children[j].className = tetrisData[i][j] >= 10 ? [tetrisData[i][j] / 10 - 1] : [tetrisData[i][j] - 1];
+            } 
+        })
+    })
+}
+
+// 문제2. 
+function drawNext() {
+    const nextTable = document.querySelector('.next-table');
+    nextTable.querySelectorAll('tr').forEach((col, i) => {
+        Array.from(col.children).forEach((row,j) => {
+            if(nextBlock.shape[0][i] && nextBlock.shape[0][i][j] > 0) {
+                nextTable.querySelectorAll('tr')[i].children[j].className = colors[nextBlock.numCode - 1];
+            }
+            else {
+                nextTable.querySelectorAll('tr')[i].children[j].className = 'white';
+            }
+        })
+    })
+}
+
+function generate() {
+    if(!currentBlock) {
+        currentBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    }
+    else {
+        currentBlock = nextBlock;
+    }
+    currentBlock.currentShapeIndex = 0;
+    nextBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    drawNext();
+    currentTopLeft = [-1,3];
+    let isGameOver = false;
+
+    currentBlock.shape[0].slice(1).forEach((col,i) => {
+        col.forEach((row,j) => {
+            if(row && tetrisData[i][j+3]) {
+                isGameOver = true;
+            }
+        });
+    });
+
+    currentBlock.shape[0].slice(1).forEach((col,i) => {
+        col.forEach((row,j) => {
+            if(row) {
+                tetrisData[i][j+3] = currentBlock.numCode;
+            }
+        });
+    });
+
+    if(isGameOver) {
+        clearInterval(int);
+        draw();
+        alert('종료');
+    }
+    else {
+        draw();
+    }
+}
+
+let int = setInterval(tick, 2000);
+init();
+generate();
