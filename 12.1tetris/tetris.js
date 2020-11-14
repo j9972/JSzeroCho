58516e029a3ed3b110e2a9f1e560998a75f4e3fd
j9972 <j9972@naver.com>
2020-11-14 19:18:31 +0900

    일단락

--- 12.1tetris/tetris.js
+++ 12.1tetris/tetris.js
@@ -200,3 +200,99 @@ const blocks = [
 ];
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'violet'];
+
+const IsActiveBlock = value => (value > 0 && value < 10);
+const IsInvalidBlock = value => (value === undefined || value >= 10);
+
+function init() {
+    const fragment = document.createDocumentFragment();
+    [...Array(20).fill()].forEach((col,i) => {
+        const tr = document.createElement('tr');
+        fragment.appendChild(tr);
+        [...Array(10).fill()].forEach((row,j) => {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+        })
+        const column = Array(10).fill(0);
+        tetrisData.push(column);
+    });
+    tetrisData.appendChild(fragment);
+}
+
+function draw() {
+    tetrisData.forEach((col,i) => {
+        col.forEach((row,j) => {
+            if(row > 0) {
+                tetris.children[i].children[j].className = tetrisData[j][j] >= 10 ? colors[tetrisData[j][j] / 10 - 1] : colors[tetrisData[j][j] - 1];
+            }
+            else {
+                tetris.children[i].children[j].className = '';
+            }
+        })
+    })
+}
+
+function drawNext() {
+    const nextTable = document.querySelector('#next-table');
+    nextTable.querySelectorAll('tr').forEach((col,i) => {
+        Array.from(col.children).forEach((row,j) => {
+            if(nextBlock.shape[0][i] && nextBlock.shape[0][i][j] > 0) {
+                nextTable.querySelectorAll('tr')[i].children[j] = colors[numCode - 1];
+            }
+            else {
+                nextTable.querySelectorAll('tr')[i].children[j] = 'white';
+            }
+        })
+    })
+}
+
+function generate() {
+    if(!currentBlock) {
+        currentBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    } else {
+        currentBlock = nextBlock;
+    }
+    currentBlock.currentShapeIndex = 0;
+    nextBlock = blocks[Math.floor(Math.random() * blocks.length)];
+    drawNext();
+    let isGameOver = false;
+    currentTopLeft = [-1,3];
+
+    currentBlock.shape[0].slice(1).forEach((col,i) => {
+        col.forEach((row,j) => {
+            for(let k = 0; k < 10; k++) { // 이 부분으로 맨 윗부분에서 만나면 멈추는거
+                if(row && tetrisData[i][j+k]) {
+                    isGameOver = true;
+                }
+            }
+        })
+    });
+
+    currentBlock.shape[0].slice(1).forEach((col,i) => {
+        col.forEach((row,j) => {
+            if(row) {
+                tetrisData[i][j+3] = currentBlock.numCode;
+            }
+        })
+    });
+
+    if(isGameOver) {
+        clearInterval(int);
+        draw();
+        alert('over');
+    }else {
+        draw();
+    }
+}
+
+function checkRows() {
+
+}
+
+function tick() {
+
+}
+
+let int = setInterval(tick, 2000);
+init();
+generate();
