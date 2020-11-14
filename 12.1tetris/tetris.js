69a9dcc9c485c017b1af5a55b33592d41103a8b8
j9972 <j9972@naver.com>
2020-11-14 16:43:34 +0900

    테트리스 기본 세팅

---
+++ 12.1tetris/tetris.js
@@ -0,0 +1,202 @@
+const tetris = document.querySelector('#tetris');
+let tetrisData = [];
+let currentBlock;
+let nextBlock;
+let currentTopLeft = [0, 3];
+const blocks = [
+  {
+    name: 's', // 네모
+    center: false,
+    numCode: 1,
+    color: 'red',
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0],
+        [0, 1, 1], // 1 부분이 도형의 모양
+        [0, 1, 1],
+      ]
+    ],
+  },
+  {
+    name: 't', // T자
+    center: true,
+    numCode: 2,
+    color: 'orange',
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 1, 0],
+      ],
+    ]
+  },
+  {
+    name: 'z', // 지그재그
+    center: true,
+    numCode: 3,
+    color: 'yellow',
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1],
+      ],
+      [
+        [0, 1, 0],
+        [1, 1, 0],
+        [1, 0, 0],
+      ],
+      [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+      ],
+      [
+        [0, 0, 1],
+        [0, 1, 1],
+        [0, 1, 0],
+      ],
+    ]
+  },
+  {
+    name: 'zr', // 반대 지그재그
+    center: true,
+    numCode: 4,
+    color: 'green',
+    startRow: 1,
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0],
+      ],
+      [
+        [1, 0, 0],
+        [1, 1, 0],
+        [0, 1, 0],
+      ],
+      [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0],
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1],
+      ],
+    ]
+  },
+  {
+    name: 'l', // L자
+    center: true,
+    numCode: 5,
+    color: 'blue',
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0],
+      ],
+      [
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+      ],
+      [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0],
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1],
+      ],
+    ]
+  },
+  {
+    name: 'lr', // 반대 L자
+    center: true,
+    numCode: 6,
+    color: 'navy',
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 1],
+      ],
+      [
+        [0, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0],
+      ],
+      [
+        [1, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
+      ],
+      [
+        [0, 1, 1],
+        [0, 1, 0],
+        [0, 1, 0],
+      ],
+    ]
+  },
+  {
+    name: 'b', // 1자
+    center: true,
+    numCode: 7,
+    color: 'violet',
+    currentShapeIndex: 0,
+    shape: [
+      [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+      ],
+      [
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 0, 0],
+      ],
+      [
+        [0, 0, 0, 0],
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      [
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 0],
+      ],
+    ]
+  },
+];
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'violet'];
